dfbe64eb3278a8d5453ea921a453c3d1980b2bfe
pl-1-a-anshikaa <anshikaagoyalll@gmail.com>
2026-09-13 13:58:35 +0530

    Add files via upload

---
+++ aa16.c
@@ -0,0 +1,25 @@
+Program (16) -> Write a program to generate the multiplication table of any number using while loop, do while loop and for loop
+
+Solution (16)- 
+
+#include <stdio.h>
+
+int main()
+
+(
+
+int num, i;
+
+printf("Enter a number: ");
+scanf("%d", &num);
+
+printf("\n Multiplication Table of %d\n", num):
+
+for (i=1;i<10;i++)
+
+{
+printf("%d x %d = %d\n", num, i, num " i);
+}
+
+return 0;
+}
